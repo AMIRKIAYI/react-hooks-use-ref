e5272ab49514b34a8f5907896724fdf1d4bd4e11
AMIRKIAYI <amirkiayi516@gmail.com>
2024-08-02 07:42:51 +0300

    update file

--- src/components/Ticker.js
+++ src/components/Ticker.js
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { makeRandomNumber } from "../utils";
+
 
 function Ticker() {
   const [price, setPrice] = useState(0);
   const [color, setColor] = useState("black");
+  const PrevpriceRef = useRef(price)
 
   useEffect(() => {
     const id = setInterval(() => setPrice(makeRandomNumber), 1000);
@@ -12,6 +14,18 @@ function Ticker() {
     };
   }, []);
 
+  useEffect(() => {
+    const Prevprice = PrevpriceRef.current;
+    if (price > Prevprice) {
+      setColor("green");
+      } else if (price < Prevprice) {
+        setColor("red");
+        } else {
+          setColor("black");
+          }
+          PrevpriceRef.current = price;
+          }, [price]);
+          
   return (
     <div>
       <h1>TickerMaster</h1>
